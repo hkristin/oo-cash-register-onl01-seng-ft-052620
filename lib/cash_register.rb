f9c92eff3d29358e5c16d9802da0b083c8d1e38d
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-08 15:38:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
   end
   
   def apply_discount
-    @total -= @total * (@discount / 100.0)
+    @total.float -= @total * (@discount / 100.0)
    "After the discount, the total comes to $#{@total}."
   end
 end
