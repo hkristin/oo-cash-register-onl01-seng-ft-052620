28e3b6a60120d179e63f0aba89fc4356ae3f64d4
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-08 15:27:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,4 +9,9 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     @total += price * quantity
   end
+  
+  def apply_discount
+    @total -= @total * (@discount / 100)
+    
+  end
 end
