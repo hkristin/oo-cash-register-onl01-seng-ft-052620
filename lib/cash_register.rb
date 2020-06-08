58b7356245f7d04efda1823679a1c15b997895f7
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-08 15:45:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,10 +13,11 @@ class CashRegister
   end
   
   def apply_discount
-    @total -= @total * (@discount / 100.0)
-    if @total % 1 == 0 
-      @total = @total.to_i
-    end
-   "After the discount, the total comes to $#{@total}."
+    if @discount > 0
+      @total -= @total * (@discount / 100.0)
+      if @total % 1 == 0 
+        @total = @total.to_i
+      end
+     "After the discount, the total comes to $#{@total}."
   end
 end
